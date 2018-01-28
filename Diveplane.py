204c57e1be8ee2ffa0e642a777ceb8a48b2afa21
Bitt Faulk <wfaulk@gmail.com>
2018-01-27 19:40:46 -0500

    implemented in Python

---
+++ Diveplane.py
@@ -0,0 +1,41 @@
+seed = 1
+data = []
+probsum = 0
+
+def init(idata, iseed=1):
+	global data
+	global probsum
+	global seed
+	for key in sorted(idata, key=int):
+		#print key
+		data.append([key, idata[key]])
+		probsum += idata[key]
+	seed = iseed
+
+def rand():
+	rn = lcg() * probsum
+	i = 0
+	while rn > 0:
+		rn -= data[i][1]
+		i += 1
+	return data[i-1][0]
+
+def lcg():
+	modulus = (2**31)-1
+	multiplier = 1103515245
+	increment = 12345
+	mask = 0x3FFFFFFF
+	global seed
+
+	seed = ( ( ( multiplier * seed ) + increment ) % modulus ) & mask
+	#print seed
+
+	return seed / float(((2**30)-1))
+
+if __name__ == "__main__":
+	#d = { 1:0.25, 2:0.5, 7:0.25 }
+	import input
+	init(input.d,123)
+	for i in range(0,10000):
+		print rand()
+		#print lcg()
